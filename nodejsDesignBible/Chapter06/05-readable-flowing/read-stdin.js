220cb8074689099bb96285c27f62d0af807fd6e6
Tae Jin Jang <48776412+Ryze0323@users.noreply.github.com>
2021-12-03 00:35:11 +0900

    Delete nodejsDesignBible/Chapter06 directory

--- nodejsDesignBible/Chapter06/05-readable-flowing/read-stdin.js
+++
@@ -1,8 +0,0 @@
-process.stdin
-  .on('data', (chunk) => {
-    console.log('New data available')
-    console.log(
-      `Chunk read (${chunk.length} bytes): "${chunk.toString()}"`
-    )
-  })
-  .on('end', () => console.log('End of stream'))
--- nodejsDesignBible/Chapter06/16-connecting-streams-using-pipes/replace.js
+++
@@ -1,5 +0,0 @@
-import { ReplaceStream } from './replace-stream.js'
-
-process.stdin
-  .pipe(new ReplaceStream(process.argv[2], process.argv[3]))
-  .pipe(process.stdout)
